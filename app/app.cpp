7799b0e3631861ecbea01812c3ca0ab1aef10f0d
Joe Rohd <joerohd@gmail.com>
2016-04-06 14:32:26 -0700

    transforming to a runtime stub, not a good idea, part 2

--- app/app.cpp
+++ app/app.cpp
@@ -1,7 +1,14 @@
 // Application that uses the developed classes goes here
 #include <iostream>
+#include "LightController.hpp"
+
+using namespace Camax;
 
 int main()
 {
+    LightController myLight;
+
+    myLight.Reset();
+
     return 0;
 }
