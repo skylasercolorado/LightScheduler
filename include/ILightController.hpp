2cc02d437f56435b4ed0d9469a7ea5359d3a974c
Joe Rohd <joerohd@gmail.com>
2016-04-29 15:23:34 -0700

    adding bright et al

--- include/ILightController.hpp
+++ include/ILightController.hpp
@@ -7,17 +7,23 @@ namespace Camax
 {
     enum LightStatus
     {
-        LightIdUnknown    = -1,
-        LightStateUnknown = -2,
-        LightStateOn      = 1,
-        LightStateOff     = 2
+        LightIdUnknown          = -1,
+        LightStateUnknown       = -2,
+        LightStateOn            = 1,
+        LightStateOff           = 2,
+        LightStateBrightened    = 3,
+        LightStateDimmed        = 4,
+        LightStateStrobed       = 5
     };
 
     class ILightController
     {
     public:
-        virtual void TurnOn(uint id) = 0;
-        virtual void TurnOff(uint id) = 0;
+        virtual void TurnOn(uint id)    = 0;
+        virtual void TurnOff(uint id)   = 0;
+        virtual void Brighten(uint id)  {};
+        virtual void Dim(uint id)       {};
+        virtual void Strobe(uint id)    {};
     };
 }
 
